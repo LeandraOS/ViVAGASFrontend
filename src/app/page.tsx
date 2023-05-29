9a62a568f53ed3fc7789d22fdbccfab8fe3d0d73
leandra.silva <leandra.silva@ccc.ufcg.edu.br>
2023-05-29 03:54:41 -0300

    feature: add configs and first component: header

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+
+// @use-client
+import { createContext } from 'react';
+
+import ResponsiveAppBar from './components/AppBar'
+export default function Home() {
+  return(
+    <>
+    <ResponsiveAppBar />
+
+    
+    </>
+
+  )
+
+}
