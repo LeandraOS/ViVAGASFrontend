5f89a7fb4771d8bb5bc197e2425a7f04c405ca02
leandra.silva <leandra.silva@ccc.ufcg.edu.br>
2023-06-25 00:25:46 -0300

    feat: adiciona implemantação e página Home

---
+++ src/app/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import { Wrapper } from './styles'
+import { TitleWelcome } from '@/app/components/TitleWelcome/TitleWelcome'
+
+
+export const Home = () => {
+  return (
+    <Wrapper>
+      <TitleWelcome />
+    </Wrapper>
+  )
+}
