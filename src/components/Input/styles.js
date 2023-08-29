2104f50caa0d68e195d876fa1e61506f25a5b625
leandra.silva <leandra.silva@ccc.ufcg.edu.br>
2023-08-28 23:55:47 -0300

    adiciona nova implementação de inputSearch

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,48 +1,41 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { SearchOutlined } from '@ant-design/icons';
 
-import { Input } from 'antd'
+export const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: white;
+  border: 1px solid #2878BE;
+  border-radius: 8px;
+  padding-left: 14px;
+  margin: 64px 64px;
+  margin-left: auto;
+  width: 280px; 
 
-const { Search } = Input
+`;
 
-export const ContainerSearch = styled(Search)`
-    border: 1px solid #2878BE;
-    width: 300px;
-    border-radius: 6px;
-    :where(.css-dev-only-do-not-override-14wwjjs).ant-input-search .ant-input:hover, :where(.css-dev-only-do-not-override-14wwjjs).ant-input-search .ant-input:focus{  
-        box-shadow: none;
-        border-color: transparent;
-        outline: none;
-    }
-    input{
-        border-color: transparent;
-    }
-    input::placeholder{
-        color: #8FC9FC;
-    }
+export const SearchInput = styled.input`
+  flex: 1;
+  border: none;
+  outline: none;
+`;
 
-button{
-    background-color: #fff;
-    box-shadow: none;
+export const SearchButton = styled.button`
+  background-color: transparent;
+  border: none;
+  border-left: 1px solid #2878BE;
+  height: 42px;
+  width: 42px;
+  cursor: pointer;
+  
+`;
 
-    :where(.css-dev-only-do-not-override-14wwjjs).ant-btn-primary:not(:disabled):not(.ant-btn-disabled):hover{
-        background-color: #fff;
-    }
+export const SearchIcon = styled(SearchOutlined)`
+  color: #2878BE;
+  font-size: 1rem;
 
-    :where(.css-dev-only-do-not-override-14wwjjs).ant-btn-primary{
-        box-shadow: none;
-    }  
-    :where(.css-dev-only-do-not-override-14wwjjs).ant-btn:not(:disabled):focus-visible{
-        outline: none;
-        outline-offset: 0;
-        transition: none;
-        box-shadow: none;
-    }
-
-    svg{
-        color: #2878BE;
-        
-    }
-
-}
-
-`
+  &:hover{
+    transition: transform 0.2s ease;
+    transform: scale(1.2);
+  }
+`;
--- src/pages/Vacancy/Vagas.jsx
+++ src/pages/Vacancy/Vagas.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { CardProject } from '../../components/CardProject/CardProject'
 import { Wrapper } from './styles'
+import { SearchComponent } from '../../components/Input/InputSearch'
 
 
 export const Vagas =() => {
   return (
     <>
+    <SearchComponent />
     <Wrapper>
       <CardProject active={true} ></CardProject>
       <CardProject active={true} ></CardProject>
